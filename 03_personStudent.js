fb50d5f26aa503c12250ff9169fe901f7c1e2ba4
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-02-13 21:45:15 +0400

    fix(03_personStudent.js) fix getter and setter functions

--- 03_personStudent.js
+++ 03_personStudent.js
@@ -1,22 +1,51 @@
 class Person {
   constructor(firstName, lastName, gender, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.age = age;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._gender = gender;
+    this._age = age;
   }
 
-  getInfo() {
-    return `${this.firstName} ${this.lastName} is ${this.gender} and ${this.age} years old`;
+  get firstName() {
+    return this._firstName;
   }
-  setInfo(first, last, gender, age) {
-    this.firstName = first;
-    this.lastName = last;
-    this.gender = gender;
-    this.age = age;
+  get lastName() {
+    return this._lastName;
   }
+  get gender() {
+    return this._gender;
+  }
+  get age() {
+    return this._age;
+  }
+
+  set firstName(value) {
+    if (typeof value === 'string') {
+      this._firstName = value;
+    }
+    return this._firstName;
+  }
+  set lastName(value) {
+    if (typeof value === 'string') {
+      this._lastName = value;
+    }
+    return this._lastName;
+  }
+  set age(value) {
+    if (typeof value === 'number') {
+      this._age = value;
+    }
+    return this._age;
+  }
+  set gender(value) {
+    if (typeof value === 'string') {
+      this._gender = value;
+    }
+    return this._gender;
+  }
+
   toString() {
-    return `${this.firstName} ${this.lastName} is ${this.age} years old`;
+    return `${this._firstName} ${this._lastName} is ${this._age} years old`;
   }
 }
 
